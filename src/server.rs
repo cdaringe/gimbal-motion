bf80b07845c000058366fd300572aca44784f764
cdaringe <cdaringe@gmail.com>
2024-02-01 00:09:24 -0800

    chore: tidy

--- src/server.rs
+++ src/server.rs
@@ -57,8 +57,10 @@ pub fn start(
         let mut buf = [0; 256];
         req.read(&mut buf)?;
         let conn = req.connection().unwrap_or("unknown");
-        info!("buf: {}", &String::from_utf8(buf.to_vec())?);
-        let body: PostGcode = serde_json::from_str(&String::from_utf8(buf.to_vec())?)?;
+        let json_str_raw = String::from_utf8(buf.to_vec())?;
+        let json_str = json_str_raw.trim();
+        info!("buf: {}", &json_str);
+        let body: PostGcode = serde_json::from_str(json_str)?;
         info!("postgcode: {}", serde_json::to_string(&body)?);
         let (code, message, payload) = match GcodeParser::of_str(&body.gcode) {
             Ok(_g) => {
